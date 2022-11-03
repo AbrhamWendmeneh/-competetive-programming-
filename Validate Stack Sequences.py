8cffd71a4b1843f50fa46ba93a1380db447e48e1
AbrhamWendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2022-11-03 15:30:39 +0300

    Create Validate Stack Sequences.py

---
+++ Validate Stack Sequences.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def validateStackSequences(self, pushed, popped):
+        """
+        :type pushed: List[int]
+        :type popped: List[int]
+        :rtype: bool
+        """
+        lenP, i, j, s = len(pushed), 0, 0, 0
+
+        while i < lenP:
+
+            if ~s and popped[j] == pushed[s]:
+
+                j += 1
+
+                s -= 1
+
+            else:
+
+                s += 1
+
+                i += 1
+
+                if i < lenP: pushed[s] = pushed[i]
+
+        return not s
