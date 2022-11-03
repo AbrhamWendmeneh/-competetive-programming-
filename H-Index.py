1bd1aa2503afc9071d8c336a73a4a97909c34faf
AbrhamWendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2022-11-03 11:13:34 +0300

    Create H-Index.py

---
+++ H-Index.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        citations.sort()
+        for i, citation in enumerate(citations):
+            if citation >= n - i:
+                return n - i
+        return 0
