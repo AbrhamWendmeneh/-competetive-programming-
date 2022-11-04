9f86719620d0c66e30bcde892974a11ef48df39e
AbrhamWendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2022-11-04 17:20:10 +0300

    Create Domino pilling.py

---
+++ Domino pilling.py
@@ -0,0 +1,10 @@
+def domino_pilling(num1 ,num2):
+    if(1 < num1 <= 16 and 1 < num2 <= 16 ):
+        max_piece = (mum1 * num2) // 2
+        return max_piece
+    else:
+        return -1
+
+num1= int(input("Enter the num1 value: "))
+num2 = int(input("Enter the num2 value: "))
+print(domino_pilling(num1, num2))
