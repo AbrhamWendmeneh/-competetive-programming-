a10a4b5f80445f59139c6b3bc7acaaeee5e31c06
AbrhamWendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2022-11-04 17:23:08 +0300

    Update Domino pilling.py

--- Domino pilling.py
+++ Domino pilling.py
@@ -1,6 +1,6 @@
 def domino_pilling(num1 ,num2):
     if(1 < num1 <= 16 and 1 < num2 <= 16 ):
-        max_piece = (mum1 * num2) // 2
+        max_piece = (num1 * num2) // 2
         return max_piece
     else:
         return -1
