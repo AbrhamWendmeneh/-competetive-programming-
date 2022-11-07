5f608b913af3bb4d954e76140dd9f8a225b46a12
AbrhamWendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2022-11-07 17:05:18 +0300

    Create Longest Mountain in Array.py

---
+++ Longest Mountain in Array.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def longestMountain(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        ans = 0
+
+        i = 0
+
+        while i + 1 < len(arr):
+
+            while i + 1 < len(arr) and arr[i] == arr[i + 1]:
+
+                i += 1
+
+            increasing = 0
+
+            decreasing = 0
+
+            while i + 1 < len(arr) and arr[i] < arr[i + 1]:
+
+                increasing += 1
+
+                i += 1
+
+            while i + 1 < len(arr) and arr[i] > arr[i + 1]:
+
+                decreasing += 1
+
+                i += 1
+
+            if increasing > 0 and decreasing > 0:
+
+                ans = max(ans, increasing + decreasing + 1)
+
+        return ans
