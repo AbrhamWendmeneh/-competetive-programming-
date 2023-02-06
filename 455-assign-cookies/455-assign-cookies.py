d713805f77584993d8c7dc093e233310d56e6020
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-02-06 17:04:37 +0300

    Time: 172 ms (64.65%), Space: 15.9 MB (36.11%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -9,12 +9,12 @@ class Solution:
         s.sort()
         init_1=0
         init_2=0
-        temp=0
+        
         while init_1< len(g) and init_2< len(s):
             if s[init_2]>=g[init_1]:
-                temp+=1
+                
                 init_1+=1
             init_2+=1
-        return temp
+        return init_1
         
         
