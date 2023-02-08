ab6a7cd49862d59da71640ebbbfdd6a2af5aa28d
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-02-08 20:02:08 +0300

    Time: 33 ms (62.02%), Space: 13.8 MB (93.32%) - LeetHub

--- 319-bulb-switcher/319-bulb-switcher.py
+++ 319-bulb-switcher/319-bulb-switcher.py
@@ -1,5 +1,5 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        if n<=2:
-            return int(n**(1/2))
+        # if n<=2:
+        #     return int(n**(1/2))
         return int(n**(1/2))
