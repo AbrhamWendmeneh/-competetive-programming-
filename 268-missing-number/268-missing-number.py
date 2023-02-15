b78089735bfcb40dbcd1ce642273509c9aa3188b
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-02-15 21:47:36 +0300

    Time: 160 ms (36.68%), Space: 15.2 MB (32.17%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,23 +1,23 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # nums.sort()
+        nums.sort()
         '''low=nums[0]
         high=[-1]
         while low <=mid:
             mid= (low+high)/2
             if '''
-        # for i, j in enumerate(nums):
-        #     if j!=i:
-        #         return i
-        # return len(nums)
-        # a=[]
-        
-        
-        for i in range(min(nums),len(nums)+1):
-            if i not in nums:
+        for i, j in enumerate(nums):
+            if j!=i:
                 return i
+        return len(nums)
+        a=[]
         
-        return 0
+        
+#         for i in range(min(nums),len(nums)+1):
+#             if i not in nums:
+#                 return i
+        
+#         return 0
         
         
         ''''
