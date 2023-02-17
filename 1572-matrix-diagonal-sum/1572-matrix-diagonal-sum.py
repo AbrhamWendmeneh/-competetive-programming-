0592ac7a8fb99716bdeb5d31f734ba217ece1ab8
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-02-17 11:59:30 +0300

    Time: 118 ms (40.52%), Space: 14 MB (90.36%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,17 +1,28 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         a=0
+        # for i in range(len(mat)):
+        #     for j in range(len(mat)):
+        #         if i==j:
+        #             a+=(mat[i][j])
+        # b=mat[::-1]
+        # c=0
+        # for i in range(len(b)):
+        #     for j in range(len(b)):
+        #         if i==j:
+        #             c+=b[i][j]
+        # if len(mat[0])%2!=0:
+        #     return a+c-(mat[(len(mat)//2)][(len(mat)//2)])
+        # else:
+        #     return a+c
         for i in range(len(mat)):
-            for j in range(len(mat)):
-                if i==j:
-                    a+=(mat[i][j])
-        b=mat[::-1]
-        c=0
-        for i in range(len(b)):
-            for j in range(len(b)):
-                if i==j:
-                    c+=b[i][j]
+            a+=mat[i][i]
+        b=0
+        mat_new=mat[::-1]
+        for i in range(len(mat)):
+            b+=mat_new[i][i]
         if len(mat[0])%2!=0:
-            return a+c-(mat[(len(mat)//2)][(len(mat)//2)])
+            return a+b-(mat[(len(mat)//2)][(len(mat)//2)])
         else:
-            return a+c
+            return a+b
+        
