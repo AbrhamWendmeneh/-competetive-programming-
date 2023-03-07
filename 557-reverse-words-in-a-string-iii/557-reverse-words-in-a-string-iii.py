649173dd4142bac1f8a0c66f87aeb2a1c61dfd3e
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-07 12:36:44 +0300

    Time: 112 ms (9.27%), Space: 14.5 MB (97.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,14 +1,58 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
-        '''first splitting the string and then iterate through the array that is formed by splitting the given string then changing it to be reversed and adding empty string to it to match with the solution of in a given format'''
-        
-        emp=''
-        
-        temp=s.split()
-        
-        for i in temp:
+#         emp=''
+#         if len(s)==1:
+#             return s
+#         right=1
+#         left=0
+#         while right<len(s) and left<right:
+#             if s[right]!=' ':
+#                 right+=1
+                
+#             else:
+#                 if s[right]==' ' and s[right]!=len(s)-1:
+#                     emp+=s[left:right][::-1]+' '
+#                     left+=(right-left+1)
+#                     right+=2
+#                 elif s[right]==len(s)-1:
+#                     emp+=s[left:right][::-1]
+                
+#         return emp
+    # def reverse_words(s):
+    # Convert the string to a list of characters
+        def reverse_sublist(lst, i, j):
             
-            emp+=i[::-1]+' '
+            # Reverse the sublist between the given indices
             
-        return emp[:len(emp)-1]
+            while i < j:
+                lst[i], lst[j] = lst[j], lst[i]
+                i += 1
+                j -= 1
+
+        chars = list(s)
+
+        # Initialize two pointers
+        i, j = 0, 0
+
+        # Loop through the list of characters
+        while j < len(chars):
+            # If we encounter a space, reverse the characters between the pointers
+            
+            if chars[j] == ' ':
+                reverse_sublist(chars, i, j-1)
+                i = j + 1
+                
+            # Move the second pointer forward
+            j += 1
+
+        # Reverse the last word in the string
+        reverse_sublist(chars, i, j-1)
+        
+
+        # Convert the list of characters back to a string and return it
+        
+        return ''.join(chars)
+    
+
+        
+                
