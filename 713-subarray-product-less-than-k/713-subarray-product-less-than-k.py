2e115a35d023bd225c2e7ea6655c5b20335243c7
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-08 19:59:39 +0300

    Time: 682 ms (91.27%), Space: 16.6 MB (60.70%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
@@ -1,41 +1,34 @@
 class Solution: 
-    import math
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-#         val=0
+        # Initialize the left pointer, result, and value counter
         
-#         for i in nums:
-            
-#             if i < k:
-#                 val+=1
-#         right=1
-#         left=0
-#         while right<len(nums):
-            
-            
-            
-#             if  math.prod(nums[left:right+1])<k:
-#                 val+=1
-#                 right+=1
-                
-#             else:
-                
-#                 left+=1
-#         return val+1
         if k<2:
+            
             return 0
-        left=0
-        right=0
-        val=0
-        result=1
-        while right<len(nums):
-            
-            result*=nums[right]
-            while result>=k:
-                result/=nums[left]
-                left+=1
-            
-            val+=right-left+1
-            right+=1
-        return val
         
+        # Initialize the left pointer, result, and value counter
+        left=0
+        result=1
+        val=0
+        
+        # Loop through each element in nums
+        for right in range(len(nums)):
             
+            # Update the result to include the current element
+            result*=nums[right]
+            
+            
+            # While the result is greater than or equal to k, 
+            # divide it by the leftmost element and move the left pointer               to the right
+            while result>=k:
+                
+                result/=nums[left]
+                
+                left+=1
+                
+            # Increment the value counter by the number of subarrays that               end at the current index
+            # (i.e. the length of the window from left to right)    
+            val+=right-left+1
+        
+        # Return the total number of subarrays with a product less than k
+        return val
