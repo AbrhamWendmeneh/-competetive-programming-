65225f304a071740bdad9d94abea5545ac787db1
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-11 10:41:26 +0300

    Time: 31 ms (68.38%), Space: 14 MB (61.77%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -1,27 +1,28 @@
 class MyStack:
 
     def __init__(self):
-        
-        self.emp_stack=[]
+        self.queue=deque()
 
     def push(self, x: int) -> None:
-        
-        self.emp_stack.append(x)
+        self.queue.append(x)
 
     def pop(self) -> int:
+        '''the case that I use is that for the values to return or to push it out i try to iterate through the loop and then by removing all the values of the given data and finding the last element in the queue and append it to the first defined queue and then i return it back'''
         
-        return self.emp_stack.pop()
+        for i in range(len(self.queue)-1):
+            
+            self.queue.append(self.queue.popleft())
+            
+        return self.queue.popleft()
 
     def top(self) -> int:
-        
-        return self.emp_stack[-1]
+        return self.queue[-1]
 
     def empty(self) -> bool:
-        
-        if len(self.emp_stack)==0:            
+        if len(self.queue)==0:
             return True
-        
         return False
+
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
