dbdc4506c008f8cdb11562dff7c7f77e8036a700
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-11 11:10:13 +0300

    Time: 49 ms (59.09%), Space: 14 MB (81.83%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -2,18 +2,27 @@ class Solution:
     def minOperations(self, logs: List[str]) -> int:
         
         '''first i try to intialize empty list that is used store the elements of the stack and then it is used for to return the value of the lenght of this list first i try to use counter=0 but in this case if '../' occurs many times my couner value goes to negative and it does not do what it '''
-        counter=[]
+#         counter=[]
         
-        for i in logs:
+#         for i in logs:
             
-            if '/' in i and './' not in i and '../' not in i:
+#             if '/' in i and './' not in i and '../' not in i:
                 
-                counter.append(i)
+#                 counter.append(i)
                 
-            elif '../' in i:
+#             elif '../' in i:
                 
-                if len(counter)>0:
+#                 if len(counter)>0:
                     
-                    counter.pop()
+#                     counter.pop()
         
-        return len(counter)
+#         return len(counter)
+
+        '''but know I fix the problem that appears in the first code'''
+        counter=0
+        for  i in logs:
+            if '/' in i and './' not in i and '../' not in i:
+                counter+=1
+            elif '../' in i and counter>0:
+                counter-=1
+        return counter
