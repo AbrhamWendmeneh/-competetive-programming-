0ce80f660403531fc23e94f8c7b351c80985b66a
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-14 23:11:33 +0300

    Update 344-reverse-string.py

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         #s.reverse()
+        '''
         left=0
         right=len(s)-1
         
@@ -12,6 +13,12 @@ class Solution:
             s[right],s[left]=s[left],s[right]
             
             right-=1
-            left+=1
+            left+=1'''
+        if len(s)==1:
+            return s
+        else:
+            
+            return self.reverseString(s[1:]) + s[:1]
+       
                 
         
