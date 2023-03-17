40da5d833e4f77ed5b415beb1d2595a0281ad95f
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-17 20:49:00 +0300

    Time: 46 ms (40.35%), Space: 13.8 MB (45.34%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,8 +1,21 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         # return int(x**(1/2))
-        i=0
+        '''i=0
         while   True:
             if (i+1)*(i+1)>x:
                 return i
-            i+=1
+            i+=1'''
+        low=1
+        high=x
+        if x==0:
+            return 0
+        while low<=high:
+            mid=(low+high)//2
+            if mid**2==x:
+                return mid
+            elif mid**2<x:
+                low=mid+1
+            else:
+                high=mid-1
+        return low-1
