fac96eedf2664587b5a199d317b950259b9dddc5
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-22 20:55:06 +0300

    Time: 130 ms (38.51%), Space: 14 MB (22.91%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,6 +1,9 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
+        '''
+        if len(s1)>len(s2):
+            return False
         low=0
         high=len(s1)
         while low<(len(s2)-len(s1)+1):
@@ -11,4 +14,25 @@ class Solution:
                 low+=1
                 
                 # print(high)
+        return False'''
+        
+        if len(s1)>len(s2):
+            return False
+        s1_count=Counter(s1)
+        counter=Counter()
+        for i,j in enumerate(s2):
+            counter[j]+=1
+            # print(counter)
+            if i>=len(s1):
+                left=s2[i-len(s1)]
+                # print(left)
+                if counter[left]==1:
+                    del counter[left]
+                else:
+                    # print(counter[left])
+                    counter[left]-=1
+            if counter==s1_count:
+                return True
         return False
+        
+        
