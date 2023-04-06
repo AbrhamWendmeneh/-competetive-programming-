f8691c72b5929e3bf5adf6f18f4a19d74b0de23f
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-04-06 22:04:07 +0300

    Time: 41 ms (66.28%), Space: 14 MB (49.56%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        '''if len(nums)==1:
+            return [nums]
+        else:
+            val=[]
+            for i in range(len(nums)):
+                val_1=nums[i]
+                val_2=nums[]'''
+        res = []
+
+        def backtrack(nums, path, res):
+            if not nums:
+                res.append(path)
+            for i in range(len(nums)):
+                backtrack(nums[:i]+nums[i+1:], path+[nums[i]], res)
+        backtrack(nums, [], res)
+        return res
+    
+    '''
+    The backtrack function takes three arguments: the remaining numbers to be permuted (nums), the current              permutation (path), and the result list (res). If there are no more numbers left to pick, we can add the current        permutation to the result list. Otherwise, we iterate through the remaining numbers and recursively call                backtrack with the ith number removed from nums and added to path.
+    '''
+    
+    
