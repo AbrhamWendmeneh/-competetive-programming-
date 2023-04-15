1f996dcf60baf7152343efbbe796dc37b9f78cbc
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-04-15 21:18:09 +0300

    Time: 35 ms (65.62%), Space: 14.2 MB (25.56%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,30 +1,40 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        a=[[]]
-        for i in nums:
-            for j in range(len(a)):
-                b=a[j][:]
-                # print(b)
-                b.append(i)
-                # print(b)
-                a.append(b)
-                # print(a)
-        return a
-    
-    
+        # a=[[]]
+        # # print(len(a))
+        # for i in nums:
+        #     for j in range(len(a)):
+        #         b=a[j][:]
+        #         print(b)
+        #         b.append(i)
+        #         # print(b)
+        #         a.append(b)
+        #         # print(a)
+        # return a
+        
+        subset=[]
+
+        def backtrackHelper(subset,path,start):
+            
+            #base case 
+            subset.append(path[:])
                 
-        '''a.append(nums[i:j+1])
-        a=[[]]+a
-        return a
-        newItem = arr[j][:]
-                newItem.append(i)
-                arr.append(newItem)'''
+            for i in range(start,len(nums)):
+                
+                #choose/ makes a selection
+                path.append(nums[i])
+
+                #do recursion
+                backtrackHelper(subset,path,i+1)
+
+                #undoes the selection  
+                path.pop()
+                    
+        backtrackHelper(subset,[],0)
         
-        '''if len(nums)==1:
-            return [[],[1]]
+        return subset
         
-        # return self.subsets
-        x = self.subsets(nums[1:])
-        return x + [[nums[0]] + y for y in x]'''
+        
+        
             
     
