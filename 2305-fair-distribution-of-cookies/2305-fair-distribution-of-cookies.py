6645e83ee335617df7bc36f3e361e23eb5f9f858
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-04-15 22:33:24 +0300

    Time: 39 ms (80.50%), Space: 13.8 MB (66.55%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -7,15 +7,24 @@ class Solution:
             if start_val >= len(cookies):
                 
                 self.val_ans=min(max(bag_val),self.val_ans)
+                
             if max(bag_val)>= self.val_ans:
                 return 
+            
             for i in range(k):
-                
+                #adding value
                 bag_val[i]+=cookies[start_val]
+                
+                #calling the recursive function
                 helper_function(start_val+1)
+                
+                #removing value 
                 bag_val[i]-=cookies[start_val]
+                
+                #cheking if the bag_val is empty or not 
                 if bag_val[i]==0:
                     break
+                    
         helper_function(0)
         return self.val_ans
                 
