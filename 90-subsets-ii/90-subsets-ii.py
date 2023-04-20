5bb3fb110d3421c2a291056b809481d6e6c3070c
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-04-20 09:52:46 +0300

    Time: 45 ms (27.85%), Space: 14 MB (99.56%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -8,7 +8,7 @@ class Solution:
 #             # a.append([nums[]])
         
 #         return a
-        a=[[]]
+        '''a=[[]]
         # print(len(a))
         for i in nums:
             for j in range(len(a)):
@@ -24,4 +24,34 @@ class Solution:
                 val.append(sorted(i))
             else:
                 continue
+        return val'''
+        subset=[]
+
+        def backtrackHelper(subset,path,start):
+            
+            #base case 
+            subset.append(sorted(path[:]))
+                
+            for i in range(start,len(nums)):
+                
+                #choose/ makes a selection
+                path.append(nums[i])
+
+                #do recursion
+                backtrackHelper(subset,path,i+1)
+
+                #undoes the selection  
+                path.pop()
+                    
+        backtrackHelper(subset,[],0)
+        val=[]
+        for i in subset:
+            if i not in val:
+                val.append(i)
+            
         return val
+    
+    # first time this code returns invaid case 
+    # when I try to append the value which is sorted in to the path 
+    # it works well and it has the same code with subset with only 
+    # appendig values in sorted way 
