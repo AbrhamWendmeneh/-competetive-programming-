18780f8093135f3a207a9a04a379ea5551d34bf0
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-04-27 21:51:30 +0300

    Time: 62 ms (26.01%), Space: 17.1 MB (8.92%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,57 +1,12 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-#         emp=''
-#         if len(s)==1:
-#             return s
-#         right=1
-#         left=0
-#         while right<len(s) and left<right:
-#             if s[right]!=' ':
-#                 right+=1
-                
-#             else:
-#                 if s[right]==' ' and s[right]!=len(s)-1:
-#                     emp+=s[left:right][::-1]+' '
-#                     left+=(right-left+1)
-#                     right+=2
-#                 elif s[right]==len(s)-1:
-#                     emp+=s[left:right][::-1]
-                
-#         return emp
-    # def reverse_words(s):
-    # Convert the string to a list of characters
-        def reverse_sublist(lst, i, j):
-            
-            # Reverse the sublist between the given indices
-            
-            while i < j:
-                lst[i], lst[j] = lst[j], lst[i]
-                i += 1
-                j -= 1
-
-        chars = list(s)
-
-        # Initialize two pointers
-        i, j = 0, 0
-
-        # Loop through the list of characters
-        while j < len(chars):
-            # If we encounter a space, reverse the characters between the pointers
-            
-            if chars[j] == ' ':
-                reverse_sublist(chars, i, j-1)
-                i = j + 1
-                
-            # Move the second pointer forward
-            j += 1
-
-        # Reverse the last word in the string
-        reverse_sublist(chars, i, j-1)
+        val=s.split()
+        res=''
+        for i in val:
+            res+=i[::-1]+' '
         
-
-        # Convert the list of characters back to a string and return it
+        return res[:-1]
         
-        return ''.join(chars)
     
 
         
