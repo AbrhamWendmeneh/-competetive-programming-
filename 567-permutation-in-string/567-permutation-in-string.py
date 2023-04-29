90c616fcf2946f2b8d1e19301d1343643bb7f400
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-04-29 22:05:41 +0300

    Time: 131 ms (35.56%), Space: 16.4 MB (6.18%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,38 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        '''
         if len(s1)>len(s2):
             return False
-        low=0
-        high=len(s1)
-        while low<(len(s2)-len(s1)+1):
-            if sorted(s2[low:low+high])==sorted(s1):
-                # print(high)  
-                return True
-            else:
-                low+=1
-                
-                # print(high)
-        return False'''
         
-        if len(s1)>len(s2):
-            return False
         s1_count=Counter(s1) 
         counter=Counter()
+        
         for i,j in enumerate(s2):
+            
             counter[j]+=1
-            # print(counter)
+            
             if i>=len(s1):
                 left=s2[i-len(s1)]
+                
                 # print(left)
                 if counter[left]==1:
                     del counter[left]
+                    
                 else:
+                    
                     # print(counter[left])
                     counter[left]-=1
+                    
             if counter==s1_count:
                 return True
+            
         return False
         
         
