e7c082f83b86465e8d5953cf16f7dd8fb56cdb50
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-05-10 23:04:05 +0300

    Time: 44 ms (12.57%), Space: 16.2 MB (15.42%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -1,14 +1,20 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        # binarystr= bin(num)[2:]
+        
         # complementint= ~int(binarystr,2)
         # complementbin=bin(complementint)[3:]
         # return int(complementbin,2)
-        print(bin(39)[2:])
-        mask = (1 << num.bit_length()) - 1
-        print(mask)
-        return num^mask
+        # print(bin(39)[2:])
+        # mask = (1 << num.bit_length()) - 1
+        # print(mask)
+        # return num^mask
         
+        
+        binarystr= bin(num)[2:]
+        val=''.join(['0' if i=='1' else '1' for i in binarystr])
+        if val:
+            return int(val,2)
+        return 1
         
         
         
