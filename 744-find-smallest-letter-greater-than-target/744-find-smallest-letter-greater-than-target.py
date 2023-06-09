1d2c9baed44f5b15038cf336cd9544c003c94db9
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-06-09 22:19:04 +0300

    Time: 132 ms (58.00%), Space: 16.7 MB (45.52%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,10 +1,24 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        val=''
-        for i in letters:
-            if target <i:
-                val=i
-                break
-        if not val:
+        
+        # I have solved this question using for loop before and
+        # now I want to solve it using binary search method
+        # in which it is one of the related problem solving method
+        
+        low=0
+        high=len(letters)-1
+        
+        while low<=high:
+            middle=low+(high-low)//2
+            
+            if letters[middle]>target:
+                high=middle-1
+            else:
+                low=middle+1
+                
+        if low==len(letters):
+            
             return letters[0]
-        return val
+        
+        return letters[low]
+        
