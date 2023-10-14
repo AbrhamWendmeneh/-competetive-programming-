87919d9b682db32a64dc5a84cb87d2b833c30e3c
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-10-14 10:15:03 +0300

    Time: 59 ms (20.92%), Space: 18.7 MB (80.08%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        
+        if not root:
+            return []
+        
+        result=[]
+        queue=deque()
+        queue.append(root)
+        
+        while queue:
+            
+            level_val=[]
+            size= len(queue)
+            
+            for _ in range(size):
+                
+                curr=queue.popleft()
+                level_val.append(curr.val)
+                
+                if curr.left:
+                    
+                    queue.append(curr.left)
+                    
+                if curr.right:
+                    
+                    queue.append(curr.right)
+                    
+            result.append(sum(level_val)/len(level_val))
+            
+        return result
