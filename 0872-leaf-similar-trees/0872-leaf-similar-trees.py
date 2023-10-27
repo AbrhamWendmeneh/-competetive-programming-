907dbb2d749bc21f6db3eb8bada34384d54e3d3a
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-10-27 22:58:21 +0300

    Time: 40 ms (52.86%), Space: 16.2 MB (72.08%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -6,32 +6,36 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        if not root1:
-            return
-        result1=[]
-        stack=[root1]
-        while stack:
-            node=stack.pop()
-            if node.right is None and node.left is None:
-                result1.append(node.val)
-            if node.right:
-                stack.append(node.right)
-            if node.left:
-                stack.append(node.left)
+        if not root1 or not root2:
+            
+            return 
+        
+        def checkVal(root):
+            
+            result=[]
+            stack=[root]
+            
+            while stack:
                 
+                node= stack.pop()
+                
+                if node.left is None and node.right is None:
+                    
+                    result.append(node.val)
+                if node.right:
+                    
+                    stack.append(node.right)
+                    
+                if node.left:
+                    
+                    stack.append(node.left)
+                    
+            return result
+        
+        return checkVal(root1)== checkVal(root2)  
     
-        result2=[]
-        stack2=[root2]
-        while stack2:
-            node=stack2.pop()
-            if node.right is None and node.left is None:
-                result2.append(node.val)
-            if node.right:
-                stack2.append(node.right)
-            if node.left:
-                stack2.append(node.left)
-                
-        return result1==result2
-        
-        
+    
+'''
+this is the efficient one beside the first one 
+  '''  
         
