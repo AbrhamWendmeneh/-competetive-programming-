6785d918b60801b23cff26627d9dd68768af2b81
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-11-02 10:07:16 +0300

    Time: 51 ms (49.33%), Space: 17.5 MB (83.96%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -5,30 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def averageOfSubtree(self, root: TreeNode) -> float:
-        def countNodes(node):
-            if node is None:
-                return 0
-
-            subtree_sum, subtree_count = calculateAndCount(node)
-            average = subtree_sum // subtree_count
-
-            return (node.val == average) + countNodes(node.left) + countNodes(node.right)
-
-        def calculateAndCount(node):
-            if node is None:
-                return 0, 0
-
-            left_sum, left_count = calculateAndCount(node.left)
-            right_sum, right_count = calculateAndCount(node.right)
-
-            subtree_sum = left_sum + right_sum + node.val
-            subtree_count = left_count + right_count + 1
-
-            return subtree_sum, subtree_count
-
-        return countNodes(root)
-       
-
-            
+    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         
+        count=0
+        
+        def dfs(node):
+            
+            nonlocal count
+            
+            if not node:
+                
+                return 0,0
+            
+            leftsum, leftcount= dfs(node.left)
+            
+            rightsum, rightcount=dfs(node.right)
+            
+            sumval= leftsum+node.val+rightsum
+            
+            sumcount= leftcount+rightcount+1 
+            
+            if sumval//sumcount==node.val:
+                
+                count+=1
+                
+            return sumval, sumcount
+        
+        dfs(root)
+        
+        return count
