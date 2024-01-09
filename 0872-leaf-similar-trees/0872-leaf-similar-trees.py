d2f4633aa3b70dab551846c57cd153ce4c366f84
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2024-01-09 10:03:23 +0300

    Time: 40 ms (53.64%), Space: 17.5 MB (7.47%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -6,36 +6,44 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        
         if not root1 or not root2:
             
             return 
         
-        def checkVal(root):
+        def helper(root):
             
-            result=[]
-            stack=[root]
+            result = []
+            
+            stack = [root]
             
             while stack:
                 
-                node= stack.pop()
+                node = stack.pop()
                 
-                if node.left is None and node.right is None:
+                if node.right is None and node.left is None:
                     
                     result.append(node.val)
-                if node.right:
-                    
-                    stack.append(node.right)
                     
                 if node.left:
                     
                     stack.append(node.left)
                     
+                if node.right:
+                    
+                    stack.append(node.right)
+         
             return result
         
-        return checkVal(root1)== checkVal(root2)  
-    
-    
-'''
-this is the efficient one beside the first one 
-  '''  
+                    
+        val1 = helper(root1)
+        
+        val2 = helper(root2)
+        
+        return val1 == val2
+                    
+                
+                
+        
+
         
