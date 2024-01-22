07dc353fe62a14177ce7ca6718f4b5db965b4937
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2024-01-22 09:42:10 +0300

    Time: 149 ms (94.83%), Space: 18.6 MB (51.72%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,35 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         
-        dict_val = {}
-        result = []
+        n = len(nums)
         
-        for i,val in enumerate(nums):
-            
-            if i+1 in dict_val:
-                
-                dict_val[i+1] += 1
-            else:
-            
-                dict_val[i+1] = 0
-
-        for val in nums:
-            
-            if val in dict_val:
-                
-                dict_val[val] += 1
-    
+        expected_sum = n*(n+1)//2
         
-        for i in dict_val:
-            
-            if dict_val[i] >1:
-                
-                duplicate = i
-                
-            elif dict_val[i] == 0:
-                
-                missing = i
-                
-        return [duplicate, missing]
+        actual_sum = sum(nums)
+        
+        duplicate = actual_sum - sum(set(nums))
+        
+        missing_val = expected_sum - actual_sum + duplicate
+        
+        return [duplicate, missing_val]
+        
         
         
