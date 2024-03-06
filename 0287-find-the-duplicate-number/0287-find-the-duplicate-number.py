95b90e3b58dc6496d09fb6f96ca02f5403aa2f2b
Abrham Wendmeneh <wendmeneha@gmail.com>
2024-03-06 14:04:58 +0300

    Time: 444 ms (91.90%), Space: 32.5 MB (25.78%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        set_val = set()
+        
+        for i in nums:
+            
+            if i in set_val:
+                
+                return i
+            
+            else:
+                
+                set_val.add(i)
+        
+            
+                
+                
+       
+        
+        
