24b7c7386ddb7ae0b96decd6564369fe1d4e0de1
Abrham Wendmeneh <wendmeneha@gmail.com>
2024-03-23 14:23:11 +0300

    Time: 33 ms (73.54%), Space: 16.5 MB (53.19%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,36 +1,24 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        # max_len=max(len(word1),len(word2))
-        # res=''
-        # for i in range(max_len):
-        #     if len(word1)-1<i:
-        #         continue
-        #     res+=word1[i]
-        #     if len(word2)-1<i:
-        #         continue
-        #     res+=word2[i]
-        # return res
-        right=0
-        left=0
-        res=''
         
-        #this is for the condition the two strings are less than to the value of thier corrspondant string values
-        while left< len(word1) and right<len(word2):
-            
-            res+=word1[left]+ word2[right]
-            right+=1
-            left+=1
-            
-        #this for if one of the string size is lessthan to compete the value with the other string value in which it gives index out of range result if we try to cosider the two     
-        while left<len(word1):
-            
-            res+=word1[left]
-            left+=1
-        #the same type to the above one only the values are different but they have same case   
+        result = ''
+        i = 0
         
-        while right<len(word2):
+        while i < len(word1) and i < len(word2):
             
-            res+=word2[right]
-            right+=1
+            result += word1[i]
             
-        return res
+            result += word2[i]
+            
+            i += 1
+            
+        if i < len(word1):
+            
+            result += word1[i:]
+            
+        if i < len(word2):
+             
+            result += word2[i:]
+            
+        return result
+        
