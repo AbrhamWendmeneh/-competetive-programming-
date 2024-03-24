e52f44fc01a8b445aa1d9e3591059b5d774d38b1
Abrham Wendmeneh <wendmeneha@gmail.com>
2024-03-24 11:36:25 +0300

    Time: 42 ms (17.53%), Space: 16.7 MB (40.35%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,30 +1,25 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         
-        '''
-        This question is smilar with question # 1143
-        Longest Common Subsequence and I use the same method 
-        their difference is that they method of returning between them 
-        in this case we can compare to the value of the 
-        longest subsquence with the length of the given string 's'
-        and if it the same we can simply return True else return False
+        pointer1 = 0      
+        pointer2 = 0
         
-        '''
+        check = [0] * len(s)
         
-        m, n = len(s), len(t)
-        dp= [[0] * (n + 1) for _ in range(m + 1)]
-        
-        for i in range(1,m+1):
+        while pointer1 < len(t) and pointer2 < len(s):
             
-            for j in range(1,n+1):
+            if s[pointer2] == t[pointer1] :
                 
-                if s[i-1]==t[j-1]:
-                    
-                    dp[i][j]=dp[i-1][j-1]+1
-                    
-                else:
-                    
-                    dp[i][j]= max(dp[i-1][j],dp[i][j-1])
-                    
-        return dp[-1][-1] == m
+                check[pointer2] = 1
+                pointer2 += 1
+                pointer1 += 1
+                
+            else:
+                
+                pointer1 += 1
+                
+        return check == ([1]*(len(s)))
+                
             
+                
+                
